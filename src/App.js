3045427fa957fb4cb393a4ae59d09e563f7cd8ba
olhaSlipushchenko <103112284+olhaSlipushchenko@users.noreply.github.com>
2022-08-14 00:18:48 +0300

    Search engine created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         hello 
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>
+        Coded by Olha S
+        </small>
+      </footer>
+      </div>
     </div>
   );
 }
